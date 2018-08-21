deb4f8946cf82c59f2d5ec9d5b404cb9a57690bd
Sav Younts <savyounts@gmail.com>
2018-08-21 17:04:27 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES (Mr.Chocolate, 2, M, brown, nice, true);
  
