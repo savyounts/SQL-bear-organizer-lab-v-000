9ced0b7017eda62d72428af31785b2ea2be266a3
Sav Younts <savyounts@gmail.com>
2018-08-21 17:04:46 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES (Mr.Chocolate, 2, M, brown, nice, true);
- 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Mr.Chocolate, 2, M, brown, nice, true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Mr.Chocolate, 2, M, brown, nice, true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Mr.Chocolate, 2, M, brown, nice, true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Mr.Chocolate, 2, M, brown, nice, true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Mr.Chocolate, 2, M, brown, nice, true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Mr.Chocolate, 2, M, brown, nice, true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Mr.Chocolate, 2, M, brown, nice, true);
